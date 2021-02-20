9ab1fe053d7b05fd70cf41d7858e50c973e88f98
sifactlbs <sifa.catalbas@keove.com>
2021-02-20 14:17:27 +0300

    Initial Commit

---
+++ weatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weatherApp
+//
+//  Created by Yasemin YEL on 20.02.2021.
+//  Copyright © 2021 Sifa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
